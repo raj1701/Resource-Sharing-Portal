e8bdad220fc452d8bbb05243ab4cbec9601f98b7
raj1701 <rajatpartani@gmail.com>
2020-12-31 14:46:31 +0530

    Deployed AWS Cloud
    Signed-off-by: raj1701 <rajatpartani@gmail.com>

--- resourceportal/settings.py
+++ resourceportal/settings.py
@@ -147,6 +147,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_HOST = 'https://d10gxcejxssdth.cloudfront.net' if not DEBUG else ''
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
